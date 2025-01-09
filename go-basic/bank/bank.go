197cf477b9ba09ea06fa829695c0770fa64f22e1
Tung <tungphann.work@gmail.com>
2025-01-09 15:08:31 +0200

    bank exercise

--- go-basic/bank/bank.go
+++ go-basic/bank/bank.go
@@ -1,45 +1,62 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
+func writeDataToFile(balance float64) {
+	balanceText := fmt.Sprint(balance)
+	os.WriteFile("balance.txt", []byte(balanceText), 0644)
+}
 func main() {
 
 	var balance = 10000
 	fmt.Println("Welcome to TP bank.")
-	fmt.Println("What do you want ?")
 
-	fmt.Println("1. Check balance.")
-	fmt.Println("2. Deposit money.")
-	fmt.Println("3. Withdraw money.")
-	fmt.Println("4. Exit.")
+	for {
+		fmt.Println("What do you want ?")
 
-	var choice int
-	fmt.Println("Your choice: ")
-	fmt.Scan(&choice)
-	fmt.Println("Your choice: ", choice)
+		fmt.Println("1. Check balance.")
+		fmt.Println("2. Deposit money.")
+		fmt.Println("3. Withdraw money.")
+		fmt.Println("4. Exit.")
 
-	switch choice {
+		var choice int
+		fmt.Println("Your choice: ")
+		fmt.Scan(&choice)
+		fmt.Println("Your choice: ", choice)
 
-	case 1:
-		fmt.Println("Your balance: ")
-		fmt.Println(balance)
-	case 2:
-		var deposit int
-		fmt.Println("Enter amount to deposit: ")
-		fmt.Scan(&deposit)
-		balance += deposit
-	case 3:
-		var withdraw int
-		fmt.Println("Enter amount to withdraw: ")
-		fmt.Scan(&withdraw)
-		if withdraw > balance {
-			fmt.Println("Insufficient balance.")
-		} else {
-			balance -= withdraw
+		switch choice {
+
+		case 1:
+			fmt.Println("Your balance: ")
+			fmt.Println(balance)
+		case 2:
+			var deposit int
+			fmt.Println("Enter amount to deposit: ")
+			fmt.Scan(&deposit)
+			if deposit < 0 {
+				fmt.Println("Invalid deposit amount.")
+				continue
+			}
+			balance += deposit
+			writeDataToFile(float64(balance))
+		case 3:
+			var withdraw int
+			fmt.Println("Enter amount to withdraw: ")
+			fmt.Scan(&withdraw)
+			if withdraw > balance {
+				fmt.Println("Insufficient balance.")
+				continue
+			} else {
+				balance -= withdraw
+				writeDataToFile(float64(balance))
+			}
+		default:
+			fmt.Println("Thank you for using TP bank.")
+			return
 		}
-	case 4:
-		fmt.Println("Thank you for using TP bank.")
-		return
 	}
 
 }
