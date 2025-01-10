f633d11c35312f5b20338dca27e980ed03eb7e42
Tung <tungphann.work@gmail.com>
2025-01-10 13:37:27 +0200

    error handling and write to file

--- go-basic/profit-calculator/profit_calculator.go
+++ go-basic/profit-calculator/profit_calculator.go
@@ -1,26 +1,61 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"os"
+)
 
 func main() {
-	var revenue float64
-	var expenses float64
-	var taxRate float64
 
-	fmt.Print("Enter revenue: ")
-	fmt.Scan(&revenue)
+	revenue, err := getUserInput("Revenue: ")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	expenses, err1 := getUserInput("Expenses: ")
+	if err1 != nil {
+		fmt.Println(err1)
+		return
+	}
+	taxRate, err2 := getUserInput("Tax Rate: ")
+	if err2 != nil {
+		fmt.Println(err2)
+		return
+	}
 
-	fmt.Print("Enter expenses: ")
-	fmt.Scan(&expenses)
+	ebt, profit, ratio := calculateFinancials(revenue, expenses, taxRate)
 
-	fmt.Print("Enter tax rate (in percentage): ")
-	fmt.Scan(&taxRate)
+	fmt.Printf("%.1f\n", ebt)
 
-	var earningB4Tax = revenue - expenses
-	var earningAfterTax = earningB4Tax - (earningB4Tax * taxRate / 100)
-	var ratio = earningB4Tax / earningAfterTax
+	fmt.Printf("%.1f\n", profit)
+	fmt.Printf("%.3f\n", ratio)
+	writeDataToFile(ebt, profit, ratio)
+}
 
-	fmt.Printf("Earnings before tax: $%.2f\n", earningB4Tax)
-	fmt.Printf("Earnings after tax: $%.2f\n", earningAfterTax)
-	fmt.Printf("Rato: %.2f%%\n", ratio)
+func calculateFinancials(revenue, expenses, taxRate float64) (float64, float64, float64) {
+	ebt := revenue - expenses
+	profit := ebt * (1 - taxRate/100)
+	ratio := ebt / profit
+	return ebt, profit, ratio
+}
+
+func writeDataToFile(ebt, profit, ratio float64) {
+	ebtFixed := fmt.Sprint(ebt)
+	profitFixed := fmt.Sprint(profit)
+	ratioFixed := fmt.Sprint(ratio)
+
+	data := fmt.Sprintf("ebt is %s, profit is %s, ratio is %s.\n", ebtFixed, profitFixed, ratioFixed)
+	os.WriteFile("data.txt", []byte(data), 0644)
+}
+
+func getUserInput(infoText string) (float64, error) {
+	var userInput float64
+	fmt.Print(infoText)
+	fmt.Scan(&userInput)
+
+	if userInput <= 0 {
+		return userInput, errors.New("invalid input. please enter a positive number.")
+	}
+	return userInput, nil
 }
