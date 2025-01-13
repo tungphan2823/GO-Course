dbf74ffecdc08605365d0d0193da0a7d82e6a057
Tung Phan <tungphann.work@gmail.com>
2025-01-13 13:26:11 +0200

    add metadata to struct

--- struct-practice/note/note.go
+++ struct-practice/note/note.go
@@ -11,9 +11,9 @@ import (
 )
 
 type Note struct {
-	Title     string
-	Content   string
-	CreatedAt time.Time
+	Title     string    `json: "title"`
+	Content   string    `json: "content"`
+	CreatedAt time.Time `json: "created_at"`
 }
 
 func (note Note) Display() {
